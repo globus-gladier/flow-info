43d67d3f71190accddbe642884656885c02d2739
Nickolaus D. Saint <nick@globus.org>
2024-11-19 15:07:18 -0800

    refactor!: Refactor flow info into separate flies, add caching, add client

---
+++ flow_info/cli.py
@@ -0,0 +1,96 @@
+import os
+import logging
+
+import typer
+import humanize
+from rich.console import Console
+from rich.table import Table
+import plots
+from flows_cache import FlowsCache
+from flow_info import FlowInfo
+
+log = logging.getLogger(__name__)
+app = typer.Typer(no_args_is_help=True)
+console = Console()
+
+
+@app.command()
+def summary(name: str = "xpcs"):
+    fc = FlowsCache(name)
+    items = ["name", "runs", "flows", "last_run", "run_logs_size", "cache_up_to_date"]
+    table = Table(*items)
+    summary = fc.summary()
+
+    summary["last_run"] = summary["last_run"].strftime("%A %d. %B %Y")
+    summary["runs"] = (
+        f"{summary['runs']} ({humanize.naturalsize(summary['runs_size'])})"
+    )
+    summary["flows"] = (
+        f"{summary['flows']} ({humanize.naturalsize(summary['flows_size'])})"
+    )
+    summary["run_logs_size"] = (
+        f"Runs log Cache: {humanize.naturalsize(summary['run_logs_size'])}"
+    )
+    summary["cache_up_to_date"] = str(summary["cache_up_to_date"])
+
+    table.add_row(*[summary[name] for name in items])
+    console.print(table)
+
+
+@app.command()
+def update(name: str = "xpcs"):
+    fc = FlowsCache(name)
+    console.print("Updating Flows...")
+    fc.update_flows()
+    console.print("Updating Runs...")
+    fc.update_runs()
+
+
+@app.command()
+def usage(name: str = "xpcs"):
+    fi = FlowInfo(name)
+    fi.load()
+    fi.describe_usage()
+
+
+@app.command()
+def runtimes(name: str = "xpcs"):
+    fi = FlowInfo(name)
+    fi.load()
+    fi.describe_runtimes()
+
+
+@app.command()
+def histogram(name: str = "xpcs"):
+    fi = FlowInfo(name)
+    fi.load()
+    plots.plot_histogram(fi.flow_logs)
+
+
+@app.command()
+def gantt(name: str = "xpcs"):
+    fi = FlowInfo(name)
+    fi.load()
+    plots.plot_gantt(fi.flow_logs, fi.flow_order)
+
+
+@app.command()
+def update_logs(name: str = "xpcs"):
+    fc = FlowsCache(name)
+    for run in fc.runs:
+        console.log(f"Updating run logs for run id {run_id}")
+        fc.get_run_logs(run["run_id"])
+
+
+if __name__ == "__main__":
+    # Log stuff in here
+    logger = logging.getLogger("root")
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    logger.addHandler(ch)
+    # SDK Logs are so darn chatty...
+    sdk = logging.getLogger("globus_sdk")
+    sdk.setLevel(logging.WARNING)
+
+    app()
--- flow_info/flow_info.py
+++ flow_info/flow_info.py
@@ -1,41 +1,28 @@
+import logging
 import pandas as pd
 
-from globus_automate_client import create_flows_client
+from flows_cache import FlowsCache
+
+
+log = logging.getLogger(__name__)
+
 
 class FlowInfo:
     """A class to inspect and describe Globus Flow runs.
     """
 
-    def __init__(self):
-        self.fc = create_flows_client()
-        self.flow_id = None
-        self.flow_scope = None
-        self.flow_runs = []
+    def __init__(self, name="xpcs"):
+        self.cache = FlowsCache(name)
         self.flow_logs = {}
-        self.action_logs = None
 
-    def load(self, flow_id, flow_scope, limit=100):
+    def load(self, limit=20):
         """Load a flow's executions
 
         Args:
-            flow_id (str): The uuid of the flow
-            flow_scope (str): The globus scope of the flow
             limit (int, optional): The number of flow actions to load. Defaults 100.
         """
-        self.flow_id = flow_id
-        self.flow_scope = flow_scope
-        self.flow_runs = []
-        self.flow_order = []
-        self.action_logs = {}
-
-        self.flow_runs = self.get_flow_runs(self.flow_id, limit)
-        print(f"Found {len(self.flow_runs)} flow runs.")
-
-        self.step_types = self._get_step_types(self.flow_id)
-
-        self.flow_logs, self.action_logs = self._extract_times(flow_id, flow_scope, self.flow_runs)
-        print(f"Loaded {len(self.flow_runs)} runs.")
-
+        self.flow_logs, self.action_logs = self._extract_times(self.cache.runs[0:limit])
+        log.info(f"Loaded {limit} runs")
 
     def _get_step_types(self, flow_id):
         """Get the type associated with each step.
@@ -46,45 +33,19 @@ class FlowInfo:
         Returns:
             Dict: A dict of step name and action url
         """
-
-        flow_dfn = self.fc.get_flow(flow_id).data
-
-        flow_dfn['definition']['States']
+        flow_dfn = None
+        for flow in self.cache.flows:
+            if flow_id == flow["id"]:
+                flow_dfn = flow
+                break
+        if not flow_dfn:
+            raise ValueError(f"Could not find flow {flow_id}")
 
         steps = {}
         for x in flow_dfn['definition']['States']:
-            steps[x] = flow_dfn['definition']['States'][x]['ActionUrl']
-
+            if flow_dfn['definition']['States'][x]["Type"] == "Action":
+                steps[x] = flow_dfn['definition']['States'][x]['ActionUrl']
         return steps
-
-    def get_flow_runs(self, flow_id, limit):
-        """Get the runs of the flow using pagination.
-
-        Args:
-            flow_id (str): The flow id to use
-            limit (int): Max flows to return
-        """
-        res_runs = []
-        all_runs = []
-
-        runs = self.fc.list_flow_runs(flow_id=flow_id, role="monitor_by")
-        all_runs = all_runs + runs['actions']
-        # Iterate through pages to collect more
-        while runs['has_next_page']:
-            if len(all_runs) >= limit:
-                break
-            print('Getting more runs')
-            runs = self.fc.list_flow_runs(flow_id=flow_id, marker=runs['marker'])
-            all_runs = all_runs + runs['actions']
-        
-        # Skip flows that didn't succeed
-        for fr in all_runs:
-            if fr['status'] == "SUCCEEDED":
-                res_runs.append(fr)
-
-        # Cut down to limit size
-        res_runs = res_runs[:limit]
-        return res_runs
 
     def describe_runtimes(self):
         """Print out summary stats for each step
@@ -99,9 +60,12 @@ class FlowInfo:
 
         for step in self.flow_order:
             c = f"{step}_runtime"
+            # log.debug(self.flow_logs[c].to_string())
+            with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+                print(self.flow_logs[c])
             print(f"{step}:\t mean {int(self.flow_logs[c].mean())}s, "\
                   f"median {int(self.flow_logs[c].median())}s, "\
-                  f"std {int(self.flow_logs[c].std())}s, "\
+                #   f"std {int(self.flow_logs[c].std())}s, "\
                   f"min {int(self.flow_logs[c].min())}s, "\
                   f"max {int(self.flow_logs[c].max())}s")
 
@@ -123,7 +87,7 @@ class FlowInfo:
         print(f"funcX Time:\t mean {int(self.flow_logs['total_funcx_time'].mean())}s, "\
               f"Total {int(self.flow_logs['total_funcx_time'].sum())}s")
 
-    def _extract_times(self, flow_id, flow_scope, flow_runs):
+    def _extract_times(self, flow_runs):
         """Extract the timings from the flow logs and create a dataframe
 
         Args:
@@ -138,13 +102,20 @@ class FlowInfo:
         all_res = pd.DataFrame()
         action_logs = {}
         for flow_run in flow_runs:
+            if flow_run["status"] != "SUCCEEDED":
+                log.debug(f"Skipping run {flow_run['run_id']} due to status: {flow_run['status']}")
+                continue
+
+            step_types = self._get_step_types(flow_run['flow_id'])
+
             flow_res = {}
             flow_res['action_id'] = flow_run['action_id']
             flow_res['start'] = flow_run['start_time']
             flow_res['end'] = flow_run['completion_time']
 
-            flow_logs = self.fc.flow_action_log(flow_id, flow_scope, flow_run['action_id'], limit=100)
-            action_logs[flow_res['action_id']] = flow_logs.data['entries'][-1]
+            log.debug(f"Fetching run action logs for {flow_run['action_id']}")
+            flow_logs = self.cache.get_run_logs(flow_run['action_id'])
+            action_logs[flow_res['action_id']] = flow_logs['entries'][-1]
             
             # Get step timing info. this gives a _runtime field for each step
             flow_steps, flow_order = self._extract_step_times(flow_logs)
@@ -156,7 +127,7 @@ class FlowInfo:
             flow_res.update(bytes_transferred)
 
             # Get the list of funcx task uuids to later track function execution time
-            funcx_task_ids = self._extract_funcx_info(flow_logs)
+            funcx_task_ids = self._extract_funcx_info(flow_logs, step_types)
             flow_res['funcx_task_ids'] = funcx_task_ids
 
             flowdf = pd.DataFrame([flow_res])
@@ -166,22 +137,22 @@ class FlowInfo:
                 if 'start' in c or 'end' in c:
                     flowdf[c] = (pd.to_datetime(flowdf[c]).dt.tz_localize(None) - pd.Timestamp("1970-01-01")) / pd.Timedelta('1s')
             
-            # Get a list of all the fx 
-            fx_steps = []
-            for step, ap in self.step_types.items():
-                if 'funcx' in ap:
-                    fx_steps.append(step)
-            total_fx_time = 0
+            # Get a list of all the compute
+            compute_steps = []
+            for step, ap in step_types.items():
+                if ap == "https://compute.actions.globus.org":
+                    compute_steps.append(step)
+            total_compute_time = 0
 
-            # Compute runtime fields and add together fx total time
+            # Compute runtime fields and add together compute total time
             flowdf['flow_runtime'] = flowdf['end'] - flowdf['start']
             for step in flow_order:
                 flowdf[f'{step}_runtime'] = flowdf[f'{step}_end'] - flowdf[f'{step}_start']
-                if step in fx_steps:
-                    total_fx_time += flowdf[f'{step}_runtime']
-            flowdf['total_funcx_time'] = total_fx_time
+                if step in compute_steps:
+                    total_compute_time += flowdf[f'{step}_runtime']
+            flowdf['total_funcx_time'] = total_compute_time
 
-            all_res = all_res.append(flowdf, ignore_index=True)
+            all_res = pd.concat([all_res, flowdf], ignore_index=True)
         if len(all_res) > 0:
             all_res = all_res.sort_values(by=['start'])
             all_res = all_res.reset_index(drop=True)
@@ -201,6 +172,10 @@ class FlowInfo:
 
         transfer_usage = {}
         total_bytes_transferred = 0
+        if not flow_log['details'].get('output') or not flow_log['details']['output'].get('details'):
+            log.error("Flow encountered with no bytes transferred!")
+            transfer_usage['total_bytes_transferred'] = total_bytes_transferred
+            return transfer_usage
         for k, v in flow_log['details']['output'].items():
             if 'details' in v:
                 if 'bytes_transferred' in v['details']:
@@ -210,7 +185,7 @@ class FlowInfo:
         transfer_usage['total_bytes_transferred'] = total_bytes_transferred
         return transfer_usage
 
-    def _extract_funcx_info(self, flow_logs):
+    def _extract_funcx_info(self, flow_logs: dict, step_types: dict):
         """Extract funcx task information. Work out function uuids and the runtime of all tasks
 
         Args:
@@ -221,7 +196,7 @@ class FlowInfo:
         """
 
         fx_steps = []
-        for step, ap in self.step_types.items():
+        for step, ap in step_types.items():
             if 'funcx' in ap:
                 fx_steps.append(step)
 
@@ -263,95 +238,10 @@ class FlowInfo:
         
         return res, steps
 
-    def plot_histogram(self, include=None):
-        """Create a histogram of the step runtimes
-        
-        Args:
-            include (list, optional): The list of steps to plot, e.g. ['flow', '1', '2']. Defaults to None.
-        """
-        import matplotlib.pyplot as plt
-
-        cols = []
-        labels = []
-        for c in self.flow_logs.keys():
-            if '_runtime' in c:
-                if not include or c.replace("_runtime", "") in include:
-                    cols.append(c)
-                    if c == "flow_runtime":
-                        labels.append("Total runtime")
-                    else:
-                        labels.append(c.replace("_runtime", ""))
-                    
-        df = self.flow_logs[cols]
-
-        f = plt.figure()
-        df.plot.hist(bins=20, alpha=0.5, ax=f.gca())
-        plt.legend(labels, loc='center left', bbox_to_anchor=(1.0, 0.5), fontsize=18)
-        plt.ylabel('Frequency', fontsize=18, color='black')
-        plt.xlabel('Time (s)', fontsize=18, color='black')
-        plt.tick_params(axis='both', which='major', pad=-1, labelsize=18, labelcolor='black')
-
-
-    def plot_gantt(self, limit=None, show_relative_time=True):
-        """Plot a Gantt Chart of flow runs.
-
-        Args:
-            limit (str, optional): The number of most recent flows to plot
-            show_relative_time (bool, optional): show relative time on x axis. Default: True
-        """
-        import numpy as np
-        import matplotlib.pyplot as plt
-        import seaborn as sns
-        sns.set(style='white', palette="Set2", color_codes=False)
-        sns.set_style("ticks")
-        colors = sns.color_palette('Set2')
-        
-        tasks = self.flow_logs.copy()
-        
-        # Shrink the task list to the last n rows
-        if limit:
-            tasks = tasks.tail(limit)
-            tasks = tasks.reset_index(drop=True)
-        
-        # Convert to relative time
-        if show_relative_time:
-            pd.options.display.float_format = '{:.5f}'.format
-            convert_columns = list(tasks.columns)[1:]  # Not inlcude action id
-            start = tasks['start'].min()
-            for c in convert_columns:
-                if "_runtime" not in c:
-                    tasks[c] = tasks[c] - start
-
-        # Plot from dataframe
-        fig, gnt = plt.subplots(figsize=(12, 9))
-        gnt.set_ylim(0, (len(tasks) + 1) * 10)
-        gnt.grid(True) 
-        gnt.set_yticks([(i+1) * 10 + 3 for i in range(len(tasks))]) 
-        gnt.set_yticklabels(range(len(tasks)))
-
-        for i, task in tasks.iterrows():
-            # flow_start = task['start']       
-            for j, step in enumerate(self.flow_order):
-                step_start, step_end = task[f'{step}_start'], task[f'{step}_end'] - task[f'{step}_start']
-                # if j == 0:
-                    # gnt.broken_barh([(flow_start, step_start-flow_start)], ((i+1)*10, 6), facecolor=colors[0], edgecolor='black')
-                gnt.broken_barh([(step_start, step_end)], ((i+1)*10, 6), facecolor=colors[j], edgecolor='black')
-            flow_end = task['end'] - task[f'{step}_end']
-        gnt.legend(self.flow_order,
-                    #'Flow finishing'],
-                fontsize=25, loc='upper left')
-        gnt.set_ylabel('Flow', fontsize=25, color='black')
-        gnt.set_xlabel('Time (s)', fontsize=25, color='black')
-        gnt.tick_params(axis='both', which='major', pad=-1, labelsize=25, labelcolor='black')
-
 
 if __name__ == "__main__":
-
-    flow_id = '5b91c6f4-09f5-442a-aadb-b19041cd8d6e'
-    flow_scope = f"https://auth.globus.org/scopes/{flow_id}/flow_{flow_id.replace('-','_')}_user"
-
     fi = FlowInfo()
-    fi.load(flow_id, flow_scope, limit=2)
+    fi.load(limit=10)
 
     fi.describe_runtimes()
 
